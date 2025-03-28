f4489df90195a48dfbbb58c8bd855070002e1ef2
Elena <elenabock@gmx.de>
2025-03-28 12:49:03 +0100

    implement pose estimation

---
+++ irobman-wise-2425-final-project/src/pose_estimation/pose_estimator.py
@@ -0,0 +1,157 @@
+
+import numpy as np
+import open3d as o3d
+from src.controller.ik import move_to_goal
+
+
+class PoseEstimator:
+    def __init__(self, simulation):
+        """
+        Initialize the Pose Estimator.
+
+        Args:
+            simulation: Instance of the Simulation class.
+        """
+        self.sim = simulation
+
+    def get_static_camera_data(self):
+        """
+        Capture RGB, depth, and segmentation images from the static camera.
+        """
+        rgb, depth, seg = self.sim.get_static_renders()
+        return rgb, depth, seg
+
+    def get_ee_camera_data(self):
+        """
+        Capture RGB, depth, and segmentation images from the end-effector camera.
+        """
+        rgb, depth, seg = self.sim.get_ee_renders()
+        return rgb, depth, seg
+
+    def segment_object(self, seg_image, object_id):
+        """
+        Segment the object using the segmentation image.
+
+        Args:
+            seg_image: Segmentation image from PyBullet.
+            object_id: ID of the target object.
+
+        Returns:
+            Mask of the segmented object.
+        """
+        return seg_image == object_id
+
+    def estimate_position_from_depth(self, depth_image, mask):
+        """
+        Estimate the position of the object using the depth image from a camera.
+
+        Args:
+            depth_image: Depth image from the camera.
+            mask: Binary mask of the segmented object.
+
+        Returns:
+            Position of the object center.
+        """
+        # Extract 3D points from the depth image
+        points = self.depth_to_point_cloud(depth_image, mask)
+
+        # Compute the position as the centroid of the points
+        position = np.mean(points, axis=0)
+        return position
+
+    def depth_to_point_cloud(self, depth_image, mask):
+        """
+        Convert a depth image to a 3D point cloud (in world coordinates).
+
+        Args:
+            depth_image: Depth image.
+            mask: Binary mask of the segmented object.
+
+        Returns:
+            3D point cloud of the object.
+        """
+
+        # Mask depth image
+        
+        near = 0.01
+        far = 5
+        fov = 70
+        depth_image = far * near / (far - (far - near) * depth_image)
+        depth_image = np.where(mask, depth_image, 0)
+
+        # Intrinsic camera parameters
+        f = self.sim.height / (2 * np.tan(np.radians(fov / 2)))
+        cx, cy = self.sim.width / 2, self.sim.height / 2  # Principal point
+
+        intrinsic = o3d.camera.PinholeCameraIntrinsic(width=self.sim.width, height=self.sim.height, fx=f, fy=f, cx=cx, cy=cy)
+
+        # Extrinsic camera parameters
+
+        Tc = np.array([[1,  0,  0,  0],
+                   [0,  -1,  0,  0],
+                   [0,  0,  -1,  0],
+                   [0,  0,  0,  1]]).reshape(4,4)
+        extrinsic = np.linalg.inv(np.array(self.sim.stat_viewMat).reshape((4,4),order="F")) @ Tc
+
+        # Convert depth image to point cloud
+
+        pointcloud = o3d.geometry.PointCloud.create_from_depth_image(o3d.geometry.Image(depth_image), intrinsic)#, extrinsic=np.array(self.sim.stat_viewMat).reshape((4,4),order="F"))
+
+        points = np.asarray(pointcloud.points)
+        points = np.concatenate([points, np.ones((points.shape[0], 1))], axis=-1)
+        points = np.dot(extrinsic, points.T).T # transform to world coordinates
+        points = points[:, :3]
+        """
+        # Get pixel coordinates of the mask
+        near = 0.01
+        far = 5
+        depth_image = far * near / (far - (far - near) * depth_image)
+        y, x = np.where(mask)
+        z = depth_image[y, x]
+
+        # Extrinsic camera parameters
+
+        Tc = np.array([[1,  0,  0,  0],
+                   [0,  -1,  0,  0],
+                   [0,  0,  -1,  0],
+                   [0,  0,  0,  1]]).reshape(4,4)
+        extrinsics_matrix = np.linalg.inv(np.array(self.sim.stat_viewMat).reshape((4,4),order="F")) @ Tc
+
+        # Convert to 3D points
+        x3d = (x - cx) * z / f
+        y3d = (y - cy) * z / f
+        points = np.stack([x3d, y3d, z], axis=-1)
+        points = points.reshape(-1, 3)
+
+        points = np.concatenate([points, np.ones((points.shape[0], 1))], axis=-1)
+        points = np.dot(extrinsics_matrix, points.T).T # transform to world coordinates
+        points = points[:, :3]
+        """
+        return points
+
+    def estimate_position(self, object_id):
+        """
+        Full position estimation workflow using the static and end-effector camera.
+
+        Args:
+            object_id: ID of the target object.
+
+        Returns:
+            Position of the object center.
+        """
+        # Coarse position estimation using the static camera
+        rgb_static, depth_static, seg_static = self.get_static_camera_data()
+        mask_static = self.segment_object(seg_static, object_id)
+        coarse_position = self.estimate_position_from_depth(depth_static, mask_static)
+
+        # Move end-effector camera to the coarse position
+        #orientation = np.eye(3) # rotate camera to look at the object from above        
+        #coarse_position[2] += 0.15 # add some height to view object from above
+        #move_to_goal(self.sim.robot, coarse_position)
+
+        # Refined position estimation using the end-effector camera
+        rgb_ee, depth_ee, seg_ee = self.get_ee_camera_data()
+        mask_ee = self.segment_object(seg_ee, object_id)
+        refined_position = self.estimate_position_from_depth(depth_ee, mask_ee)
+
+        return coarse_position
