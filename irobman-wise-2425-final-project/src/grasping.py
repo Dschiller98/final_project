13cbd8b88e7a4485cb2b4bef4ee1be0b22db60d1
Elena <elenabock@gmx.de>
2025-03-28 17:07:49 +0100

    implement grasp planner

--- irobman-wise-2425-final-project/src/grasping.py
+++ irobman-wise-2425-final-project/src/grasping.py
@@ -1,5 +1,7 @@
 import numpy as np
 import open3d as o3d
+import pybullet as p
+from scipy.spatial import ConvexHull
 from .ik import move_to_goal
 
 class PickAndPlace:
@@ -20,28 +22,31 @@ class PickAndPlace:
         self.robot = robot
         self.sim = sim
 
-    def pick(self, object_position: np.ndarray):
+    def pick(self, object_position: np.ndarray, orientation: np.ndarray = None):
         """
         Perform a pick operation at the specified object position.
 
         Args:
             object_position: The position of the object to pick as a numpy array [x, y, z].
         """
+        # Retrieve gripper limits
+        lower_limits, upper_limits = self.robot.get_gripper_limits()
+
+        # Open the gripper
+        self.robot.gripper_control(upper_limits)
+
         # Move above the object
         pre_grasp_position = object_position + np.array([0, 0, 0.1])  # Offset above the object
-        move_to_goal(self.robot, pre_grasp_position, np.eye(3))
+        move_to_goal(self.robot, pre_grasp_position, orientation)
 
         # Move to the object
-        move_to_goal(self.robot, object_position, np.eye(3))
-
-        # Retrieve gripper limits
-        lower_limits, _ = self.robot.get_gripper_limits()
+        move_to_goal(self.robot, object_position, orientation)
 
         # Close the gripper to grasp the object
         self.robot.gripper_control(lower_limits)
 
         # Move back up with the object
-        move_to_goal(self.robot, pre_grasp_position, np.eye(3))
+        move_to_goal(self.robot, pre_grasp_position, orientation)
 
     def place(self, goal_position: np.ndarray):
         """
@@ -52,10 +57,10 @@ class PickAndPlace:
         """
         # Move above the goal position
         pre_place_position = goal_position + np.array([0, 0, 0.1])  # Offset above the goal
-        move_to_goal(self.robot, pre_place_position, np.eye(3))
+        move_to_goal(self.robot, pre_place_position)
 
         # Move to the goal position
-        move_to_goal(self.robot, goal_position, np.eye(3))
+        move_to_goal(self.robot, goal_position)
 
         # Retrieve gripper limits
         _, upper_limits = self.robot.get_gripper_limits()
@@ -64,7 +69,7 @@ class PickAndPlace:
         self.robot.gripper_control(upper_limits)
 
         # Move back up after placing the object
-        move_to_goal(self.robot, pre_place_position, np.eye(3))
+        move_to_goal(self.robot, pre_place_position)
 
     def pick_and_place(self, object_position: np.ndarray, goal_position: np.ndarray):
         """
@@ -80,3 +85,49 @@ class PickAndPlace:
         self.place(goal_position)
         print("Object placed successfully.")
 
+
+class GraspPlanner:
+    def __init__(self, pointcloud):
+        # Convert the Open3D point cloud to a NumPy array
+        self.pointcloud = pointcloud
+
+    def compute_convex_hull(self):
+        """ Computes the convex hull of the point cloud. """
+        hull = ConvexHull(self.pointcloud)
+        return hull
+
+    def find_best_grasp(self):
+        """ Finds the best grasp configuration for a two-fingered gripper.
+        Returns the gripper center position and a rotation matrix.
+        """
+        hull = self.compute_convex_hull()
+        points = self.pointcloud[hull.vertices]
+        
+        # Compute the centroid of the convex hull
+        centroid = np.mean(points, axis=0)
+        
+        # Compute the covariance matrix and perform eigen decomposition
+        cov_matrix = np.cov(points, rowvar=False)
+        eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+        
+        # The principal axes are given by the eigenvectors corresponding to largest eigenvalues
+        idx = np.argsort(eigenvalues)[::-1]  # Sort indices by eigenvalues
+        axis1 = eigenvectors[:, idx[0]]  # First principal axis
+        axis2 = eigenvectors[:, idx[1]]  # Second principal axis
+
+        # Compute the gripper center position
+        gripper_center = centroid  # Center position for gripper
+        
+        # Create a rotation matrix aligning the first principal axis
+        x_axis = axis1
+        y_axis = axis2 / np.linalg.norm(axis2)  # Normalize the second axis
+        z_axis = np.cross(y_axis, x_axis)      # Orthogonal x-axis
+        z_axis /= np.linalg.norm(z_axis)        # Normalize x-axis
+        
+        # Final rotation matrix
+        rotation_matrix = np.array([x_axis, y_axis, z_axis]).T
+
+        # Turn the rotation matrix into a quaternion
+        quaternion = p.getQuaternionFromMatrix(rotation_matrix)
+
+        return gripper_center, quaternion
