bc0ea006c76572759cabce728db81fe997d8b3f4
Elena <elenabock@gmx.de>
2025-04-07 22:21:05 +0200

    add obstacle avoidance and check for valid grasps

--- irobman-wise-2425-final-project/src/grasping.py
+++ irobman-wise-2425-final-project/src/grasping.py
@@ -3,7 +3,6 @@ import open3d as o3d
 import pybullet as p
 from scipy.spatial import ConvexHull
 from scipy.spatial.transform import Rotation as R
-from .ik import move_to_goal
 
 class PickAndPlace:
     """
@@ -13,7 +12,7 @@ class PickAndPlace:
         robot: An instance of the Robot class.
     """
 
-    def __init__(self, sim):
+    def __init__(self, sim, controller):
         """
         Initialize the PickAndPlace class.
 
@@ -21,8 +20,9 @@ class PickAndPlace:
             robot: An instance of the Robot class.
         """
         self.sim = sim
+        self.controller = controller
 
-    def pick(self, object_position: np.ndarray, orientation: np.ndarray = None):
+    def pick(self, object_position: np.ndarray, orientation: np.ndarray = None, obstacles: bool = False):
         """
         Perform a pick operation at the specified object position.
 
@@ -35,13 +35,16 @@ class PickAndPlace:
         # Open the gripper
         self.sim.robot.gripper_control(upper_limits)
 
-        for _ in range(200):
+        for _ in range(10):
             self.sim.step()
 
         # Move to a position above the object
         pre_grasp_position = object_position + np.array([0, 0, 0.1])
-        
-        move_to_goal(self.sim.robot, pre_grasp_position, orientation)
+
+        if obstacles:
+            self.controller.move_with_obstacles(pre_grasp_position, orientation)
+        else:
+            self.controller.move_without_obstacles(pre_grasp_position, orientation)
 
         # Estimate of grasp position tends to be too high
         # Lower grasp position but make sure to stay above the table
@@ -49,18 +52,24 @@ class PickAndPlace:
         grasp_position[2] = np.max([1.24 + 0.01, grasp_position[2] - 0.04])
 
         # Move to the object
-        move_to_goal(self.sim.robot, grasp_position, orientation)
+        if obstacles:
+            self.controller.move_with_obstacles(grasp_position, orientation)
+        else:
+            self.controller.move_without_obstacles(grasp_position, orientation)
 
         # Close the gripper to grasp the object
         self.sim.robot.gripper_control(lower_limits)
 
-        for _ in range(800):
+        for _ in range(10):
             self.sim.step()
 
         # Move back up with the object
-        move_to_goal(self.sim.robot, pre_grasp_position, orientation)
+        if obstacles:
+            self.controller.move_with_obstacles(pre_grasp_position, orientation)
+        else:
+            self.controller.move_without_obstacles(pre_grasp_position, orientation)
 
-    def place(self, goal_position: np.ndarray):
+    def place(self, goal_position: np.ndarray, obstacles: bool = False):
         """
         Perform a place operation at the specified goal position.
 
@@ -69,10 +78,10 @@ class PickAndPlace:
         """
         # Move above the goal position
         pre_place_position = goal_position + np.array([0, 0, 0.2])  # Offset above the goal
-        move_to_goal(self.sim.robot, pre_place_position)
-
-        # Move to the goal position
-        #move_to_goal(self.robot, goal_position)
+        if obstacles:
+            self.controller.move_with_obstacles(pre_place_position)
+        else:
+            self.controller.move_without_obstacles(pre_place_position)
 
         # Retrieve gripper limits
         _, upper_limits = self.sim.robot.get_gripper_limits()
@@ -80,32 +89,36 @@ class PickAndPlace:
         # Open the gripper to release the object
         self.sim.robot.gripper_control(upper_limits)
 
-        for _ in range(1000):
+        for _ in range(50):
+            if obstacles:
+                # Check for obstacles and adjust the robot's position if necessary
+                current_position, current_orientation = self.sim.robot.get_ee_pose()
+                self.controller.move_with_obstacles(current_position, current_orientation)
             self.sim.step()
 
-        # Move back up after placing the object
-        #move_to_goal(self.robot, pre_place_position)
-
-    def pick_and_place(self, gripper_position: np.ndarray, grasp_orientation: np.ndarray, goal_position: np.ndarray):
+    def pick_and_place(self, gripper_position: np.ndarray, grasp_orientation: np.ndarray, goal_position: np.ndarray, obstacles: bool = False):
         """
         Execute a pick-and-place operation.
 
         Args:
-            object_position: The position of the object to pick as a numpy array [x, y, z].
+            gripper_position: The position of the gripper to pick the object as a numpy array [x, y, z].
+            grasp_orientation: The orientation of the gripper as a quaternion [x, y, z, w].
             goal_position: The position to place the object as a numpy array [x, y, z].
+            obstacles: Whether to consider obstacles during the operation.
         """
         print("Starting pick-and-place operation...")
-        self.pick(gripper_position, grasp_orientation)
-        print("Object picked successfully.")
-        self.place(goal_position)
-        print("Object placed successfully.")
+        self.pick(gripper_position, grasp_orientation, obstacles)
+        self.place(goal_position, obstacles)
 
 
 class GraspPlanner:
-    def __init__(self, pointcloud, robot):
-        # Convert the Open3D point cloud to a NumPy array
-        self.pointcloud = pointcloud
-        self.robot = robot
+    def __init__(self):
+        """ Initializes the GraspPlanner. """
+        self.pointcloud = None
+
+    def set_pcd(self, pcd):
+        """ Sets the point cloud. """
+        self.pointcloud = pcd
 
     def compute_convex_hull(self):
         """ Computes the convex hull of the point cloud. """
@@ -148,11 +161,23 @@ class GraspPlanner:
         if y_axis[0] > 0:
             x_axis = -x_axis
             y_axis = -y_axis
+
+        # Check if the z_axis is within 20 degrees of the world z-axis
+        world_z_axis = np.array([0, 0, -1])
+        dot_product = np.dot(z_axis, world_z_axis)
+        angle = np.degrees(np.arccos(np.clip(dot_product, -1.0, 1.0)))  # Clip to handle numerical precision issues
         
+        # If the angle is greater than 20 degrees, no valid grasp is found
+        if angle > 20:
+            print("No valid grasp found.")
+            return gripper_center, [0, 1, 0, 0]  # Return a default quaternion
+        
+        # Draw the axes for visualization
         p.addUserDebugLine(gripper_center, gripper_center + x_axis*0.1, [1, 0, 0], 3)
         p.addUserDebugLine(gripper_center, gripper_center + y_axis*0.1, [0, 1, 0], 3)
         p.addUserDebugLine(gripper_center, gripper_center + z_axis*0.1, [0, 0, 1], 3)
 
+        # Create a rotation matrix from the axes
         rotation = R.from_matrix(np.column_stack((x_axis, y_axis, z_axis)))
         quaternion = rotation.as_quat()
 
